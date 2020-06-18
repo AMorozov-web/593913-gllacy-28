d1df39395ebefcdecc1f7472708cab45134d09b8
Alex M <64865213+AMorozov-web@users.noreply.github.com>
2020-06-18 16:26:33 +0300

    Small bug fix

--- js/map.js
+++ js/map.js
@@ -1,45 +1,45 @@
 window.onload = function() {
     let mapLink = document.querySelector(".contacts__map");
     let contactsWidget = document.querySelector(".contacts__contact-widget");
-    mapLink.classList.add('visually-hidden');
-    contactsWidget.classList.add("widget-shadow");
-
-    ymaps.ready(init);
     var gllacyMap;
     var gllacyPlaceMark;
+
+    ymaps.ready({
+        successCallback: function () {
+            mapLink.classList.add('visually-hidden');
+            contactsWidget.classList.add("widget-shadow");
+            gllacyMap = new ymaps.Map("map", {
+                center: [59.939102, 30.326336],
+                zoom: 17,
+                controls: [],
+            }, 
+            {
+                suppressMapOpenBlock: true
+            },
+            {
+                yandexMapAutoSwitch: true
+            });
     
-    function init() {
-        gllacyMap = new ymaps.Map("map", {
-            center: [59.939102, 30.326336],
-            zoom: 17,
-            controls: [],
-        }, 
-        {
-            suppressMapOpenBlock: true
-        },
-        {
-            yandexMapAutoSwitch: true
-        });
-
-        gllacyPlaceMark = new ymaps.Placemark(
-            [59.938635, 30.323118],
-        {   
-            balloonContent: []
-        }, 
-        {
-            openBalloonOnClick: false,
-
-            iconLayout: 'default#image',
-
-            iconImageHref: '/img/map_pin.svg',
-
-            iconImageSize: [80, 140],
-
-            iconImageOffset: [-35, -170]
-
-        });
-
-        gllacyMap.behaviors.disable('scrollZoom')
-        gllacyMap.geoObjects.add(gllacyPlaceMark);
-    }
+            gllacyPlaceMark = new ymaps.Placemark(
+                [59.938635, 30.323118],
+            {   
+                balloonContent: []
+            }, 
+            {
+                openBalloonOnClick: false,
+    
+                iconLayout: 'default#image',
+    
+                iconImageHref: './img/map_pin.svg',
+    
+                iconImageSize: [80, 140],
+    
+                iconImageOffset: [-35, -170]
+    
+            });
+    
+            gllacyMap.behaviors.disable('scrollZoom')
+            gllacyMap.geoObjects.add(gllacyPlaceMark); 
+        }
+    });
 };
